77524607cc4f19b6adcedae424af7bb64ec50fdb
ruby --version <serg.casquejo@yahoo.com>
2015-03-04 00:08:42 +0800

    update

--- app/routes.php
+++ app/routes.php
@@ -8,7 +8,10 @@
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the Closure to execute when that URI is requested.
-|
+| http://screencast.com/t/7TJ0aRPMcxhD
+| http://screencast.com/t/QLQAZ8wsPWb3
+| http://screencast.com/t/Y86ytze5jBu
+| http://kmagrivet.ianquijano.com/
 */
 require_once 'routes/auth.php';
 
